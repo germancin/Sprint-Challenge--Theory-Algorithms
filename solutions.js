a920fd3eaaae26f0152784c9c06a398a44e38823
german gonzalez <gcomlnk@gmail.com>
2018-05-04 14:46:14 -0400

    regex eatherof antilopes

--- solutions.js
+++ solutions.js
@@ -1,31 +1,44 @@
+/*************************************************************************/
 // Find regexes that match the following.
 // (e.g. find a single regex that matches
 // both `antelope` and `antelopes`.)
-
+/*************************************************************************/
 const words = ['antelope', 'antelopes'];
 const res = words.map(w => (w.match(/\w+/g))? true: false );
 
-console.log('Find regexes that match the following. (e.g. find a single regex that matches\n' + 'both `antelope` and `antelopes`.)');
+console.log('1.) Find regexes that match the following. (e.g. find a single regex that matches\n' + 'both `antelope` and `antelopes`.)');
 console.log(res);
 console.log('');
 
+
+
+
+/*************************************************************************/
 // * Single regex that matches either of these:
 // antelope rocks out
 // antelopes rock out
+/*************************************************************************/
+const wordsB = ['antelope rocks out', 'antelopes rock out'];
+const resB = wordsB.map(w => (w.match(/(\w+)\s(\b\w+s\b)\s(\w+)/g))? true: false );
+
+console.log('2.) Single regex that matches either of these:antelope rocks out ,antelopes rock out');
+console.log(resB);
+console.log('');
 
 
 
 
+/*************************************************************************/
 // * Regex that matches either of:
 // goat
 // moat
 // but not:
 // boat
-
+/*************************************************************************/
 const wordsC = ['goat', 'moat', 'boat'];
 const resC = wordsC.map(w => (w.match(/(goat|moat)/g))? true: false );
 
-console.log('Regex that matches either of:');
+console.log('3.) Regex that matches either of: goat, moat {but not} boat');
 console.log(resC);
 console.log('');
 
